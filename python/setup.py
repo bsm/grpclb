e71f1adf359843249dca92807765cad9f7302011
Son Dinh <son.dinh@blacksquaremedia.com>
2017-04-06 10:08:27 +0100

    python: Bump version to 0.2.0

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='grpclb',
-    version='0.1.0',
+    version='0.2.0',
     description='grpclb contains automatically generated code for calling a grpclb server',
 
     author='Black Square Media',
