123ceca5c23906b51158838c183a3d3802265595
Max Cherry <mxmCherry@gmail.com>
2018-05-01 16:18:29 +0300

    ruby: safer approach to accept args in server constructor

--- ruby/lib/grpclb/server.rb
+++ ruby/lib/grpclb/server.rb
@@ -3,7 +3,7 @@ require 'grpclb/internal/load_report_service'
 class Grpclb::Server < GRPC::RpcServer
   DUMMY_POOL = Struct.new(:jobs_waiting).new(0)
 
-  def initialize(opts = {})
+  def initialize(*args)
     super
     handle(Grpclb::Internal::LoadReportService.new(@pool || DUMMY_POOL))
   end
